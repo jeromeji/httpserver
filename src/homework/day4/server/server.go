52d50f136c09f81bfb4bce26c4b16774a2efd9f5
jidejian <jdj914@qq.com>
2021-12-13 14:11:32 +0800

    add metrics

--- src/homework/day4/server/server.go
+++ src/homework/day4/server/server.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"github.com/cncamp/golang/httpserver/metrics"
 	"github.com/golang/glog"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"io"
+	"math/rand"
 	"net"
 	"net/http"
 	"os"
@@ -13,6 +17,11 @@ import (
 	"syscall"
 	"time"
 )
+
+func randInt(min int, max int) int {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return min + rand.Intn(max-min)
+}
 
 func RemoteIp(req *http.Request) string {
 	remoteAddr := req.RemoteAddr
@@ -58,6 +67,25 @@ func notfound(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	glog.V(4).Info("entering root handler")
+	timer := metrics.NewTimer()
+	defer timer.ObserveTotal()
+	user := r.URL.Query().Get("user")
+	delay := randInt(10, 2000)
+	time.Sleep(time.Millisecond * time.Duration(delay))
+	if user != "" {
+		io.WriteString(w, fmt.Sprintf("hello [%s]\n", user))
+	} else {
+		io.WriteString(w, "hello [stranger]\n")
+	}
+	io.WriteString(w, "===================Details of the http request header:============\n")
+	for k, v := range r.Header {
+		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
+	}
+	glog.V(4).Infof("Respond in %d ms", delay)
+}
+
 func listenSignal(ctx context.Context, httpSrv *http.Server) {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
@@ -74,7 +102,9 @@ func listenSignal(ctx context.Context, httpSrv *http.Server) {
 func main() {
 	glog.V(2).Info("....." + os.Getenv("version") + ".....")
 	flag.Parse()
+	metrics.Register()
 	http.HandleFunc("/", notfound)
+	http.HandleFunc("/root", rootHandler)
 	http.HandleFunc("/healthz", healthz)
 	http.HandleFunc("/headers", headers)
 	server := &http.Server{
